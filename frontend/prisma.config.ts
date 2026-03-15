e8b5130bff294b9819abb39da18e836ed6683d79
LucidStack-code <bhupeshlimje96@gmail.com>
2026-03-15 14:27:25 +0530

    fix: remove migrate from prisma.config.ts

--- frontend/prisma.config.ts
+++ frontend/prisma.config.ts
@@ -4,11 +4,9 @@ import * as dotenv from 'dotenv'
 
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
-const DATABASE_URL = process.env.DATABASE_URL!
-
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
   datasource: {
-    url: DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
 })
